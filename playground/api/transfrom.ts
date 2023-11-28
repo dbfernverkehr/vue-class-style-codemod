15ac9b77f80f435bffc74fd0f7f061f826b0b1f2
MelvinMcMonagle <melvin.mcmonagle@deutschebahn.com>
2023-11-28 15:22:18 +0100

    Add vue migration playground

---
+++ playground/api/transfrom.ts
@@ -0,0 +1,29 @@
+
+
+import runTransformation from '../../dist/src/runTransformation'
+
+const transfrom = process.argv[2]
+const options = JSON.parse(process.argv[3] || '{}')
+
+let input = ''
+process.stdin.on('data', (e) => {
+  input += e.toString()
+})
+
+process.stdin.on('end', () => {
+  try {
+    console.log(
+      runTransformation(
+        {
+          source: input,
+          path: 'anonymous.vue',
+        },
+        require(`../../transformations/${transfrom}.ts`),
+        options,
+        transfrom
+      )
+    )
+  } catch (e) {
+    console.error(e)
+  }
+})
